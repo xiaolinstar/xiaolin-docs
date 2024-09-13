852794ff3c87f333f1f22ec88c3f189fa5967068
xiaolinstar <xing.xiaolin@foxmail.com>
2024-09-13 07:33:38 +0000

    Deploying to gh-pages from @ xiaolinstar/xiaolin-docs@f62d73ee748696f558c66807fbe173924d51ab99 🚀

---
+++ assets/dev-exp_zk.md.CywNZC8s.lean.js
@@ -0,0 +1,19 @@
+import{_ as a,c as i,a0 as n,o as e}from"./chunks/framework.UASS0RQ-.js";const d=JSON.parse('{"title":"Zookeeper一定高可用吗","description":"","frontmatter":{},"headers":[],"relativePath":"dev-exp/zk.md","filePath":"dev-exp/zk.md"}'),p={name:"dev-exp/zk.md"};function l(t,s,o,r,h,k){return e(),i("div",null,s[0]||(s[0]=[n(`<h1 id="zookeeper一定高可用吗" tabindex="-1">Zookeeper一定高可用吗 <a class="header-anchor" href="#zookeeper一定高可用吗" aria-label="Permalink to &quot;Zookeeper一定高可用吗&quot;">​</a></h1><p>Zookeeper故障摘要</p><h2 id="背景" tabindex="-1">背景 <a class="header-anchor" href="#背景" aria-label="Permalink to &quot;背景&quot;">​</a></h2><p>Zookeeper微服务中的注册中心，原生高可用。 测试11</p><h2 id="zookeeper持久化" tabindex="-1">Zookeeper持久化 <a class="header-anchor" href="#zookeeper持久化" aria-label="Permalink to &quot;Zookeeper持久化&quot;">​</a></h2><p><strong>Input</strong></p><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">::: info</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">This is an info box.</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">::: tip</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">This is a tip.</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">::: warning</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">This is a warning.</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">::: danger</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">This is a dangerous warning.</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">::: details</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">This is a details block.</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">:::</span></span></code></pre></div><p><strong>Output</strong></p><div class="info custom-block"><p class="custom-block-title">INFO</p><p>This is an info box.</p></div><div class="tip custom-block"><p class="custom-block-title">TIP</p><p>This is a tip.</p></div><div class="warning custom-block"><p class="custom-block-title">WARNING</p><p>This is a warning.</p></div><div class="danger custom-block"><p class="custom-block-title">DANGER</p><p>This is a dangerous warning.</p></div><details class="details custom-block"><summary>Details</summary><p>This is a details block.</p></details><h2 id="宕机恢复" tabindex="-1">宕机恢复 <a class="header-anchor" href="#宕机恢复" aria-label="Permalink to &quot;宕机恢复&quot;">​</a></h2><p>Check out the documentation for the <a href="https://vitepress.dev/guide/markdown" target="_blank" rel="noreferrer">full list of markdown extensions</a>.</p>`,15)]))}const E=a(p,[["render",l]]);export{d as __pageData,E as default};
